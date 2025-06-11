9de0f0fa84566a2ea558ca838d0117beab3b62d2
kaustubhai <kaustubh229@gmail.com>
2025-06-12 00:25:15 +0530

    added dotenv

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -2,7 +2,7 @@ const pg = require("pg");
 const Pool = pg.Pool;
 const dotenv = require("dotenv");
 dotenv.config();
-console.log(process.env.DB_USER);
+console.log(process.env.DB_HOST);
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
